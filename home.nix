c81b1d3cff84ef0d456929ea1492a85a0c1f6057
shmu26 <shmuel.globus@gmail.com>
2024-03-26 06:46:28 +0200

    config

---
+++ home.nix
@@ -0,0 +1,39 @@
+#Home Manager with zsh and oh-my-zsh
+#for Plasma configuration, use konsave. To apply a profile: konsave -a <profile name>
+
+{ pkgs, ... }: {
+  home.username = "shmuel";
+  home.homeDirectory = "/home/shmuel";
+  programs.home-manager.enable = true;
+  programs.zsh.initExtra = "neofetch|lolcat";
+  home.packages = with pkgs; [
+    pkgs.oh-my-zsh
+    pkgs.neofetch
+    pkgs.lolcat
+    pkgs.zsh-autosuggestions
+     ];
+
+
+  programs.zsh = {
+   enable = true;
+   enableAutosuggestions = true;
+
+   shellAliases = {
+    ll = "ls -l";
+    bild = "sudo nixos-rebuild switch --flake ~/nixos#nixos";
+    up = "sudo nix-channel --update && sudo nixos-rebuild switch --flake ~/nixos#nixos";
+    config = "nano ~/nixos/configuration.nix";
+    hom = "nano ~/nixos/home.nix";
+
+  };
+};
+programs.zsh.oh-my-zsh = {
+  enable = true;
+  plugins = [  ];
+  theme = "agnoster";
+};
+}
+
+  #add home manager channel
+#sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+#sudo nix-channel --update
