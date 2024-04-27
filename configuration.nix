4d11b0f4157faf30b5001702bebb0ef77d5f5fa1
shmu26 <shmuel.globus@gmail.com>
2024-04-27 23:51:40 +0300

    config

--- configuration.nix
+++ configuration.nix
@@ -218,8 +218,8 @@ fileSystems."/run/media/shmuel/LinuxBackups" = {
 
   #virtualisation.virtualbox.host.enable = true;
   #virtualisation.virtualbox.host.enableExtensionPack = true;
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
+  #virtualisation.libvirtd.enable = true;
+  #programs.virt-manager.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
