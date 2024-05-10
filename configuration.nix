bee8b426eb5e0bfd33e90eb9172e5cc2f8bde359
shmu26 <shmuel.globus@gmail.com>
2024-05-10 12:28:05 +0300

    config

--- configuration.nix
+++ configuration.nix
@@ -67,6 +67,7 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.;
+  services.xserver.displayManager.sddm.enable = true; 
   services.desktopManager.plasma6.enable = true;
   services.xserver.displayManager.sddm.wayland.enable = true; 
 
