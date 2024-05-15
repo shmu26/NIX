0608fe10070cc9712645da5a5da0f50ae84a8425
shmu26 <shmuel.globus@gmail.com>
2024-05-15 15:58:35 +0300

    config

--- configuration.nix
+++ configuration.nix
@@ -49,7 +49,7 @@
   # Enable the KDE Plasma Desktop Environment.;
   services.displayManager.sddm.enable = true; 
   services.desktopManager.plasma6.enable = true;
-  services.xserver.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
    
 
    environment.plasma5.excludePackages = with pkgs.libsForQt5; [
