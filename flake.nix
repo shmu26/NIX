4b2e3a192831b1d27d8da0a8debcc4dbf6d55956
shmu26 <shmuel.globus@gmail.com>
2024-07-28 01:06:38 +0300

    config

--- flake.nix
+++ flake.nix
@@ -4,20 +4,20 @@
 inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
         # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";      
-        nix-snapd.url = "github:io12/nix-snapd";
-        nix-snapd.inputs.nixpkgs.follows = "nixpkgs";
+       # nix-snapd.url = "github:io12/nix-snapd";
+       # nix-snapd.inputs.nixpkgs.follows = "nixpkgs";
     };
 
   #for snapd, add to outputs: nix-snapd
-  outputs = { self, nixpkgs, nix-snapd }: {
+  outputs = { self, nixpkgs, }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          nix-snapd.nixosModules.default
+         # nix-snapd.nixosModules.default
          {
-           services.snap.enable = true;
+          # services.snap.enable = true;
         }
         ];
       };
